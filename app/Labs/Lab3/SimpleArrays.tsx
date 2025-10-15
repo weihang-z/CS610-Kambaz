c05985cb5e6fe0ee7b32f3a46eab6f4e67bbc62a
weihang-z <weihangzeng79@gmail.com>
2025-10-14 23:54:15 -0400

    fix deployment problems again

--- app/Labs/Lab3/SimpleArrays.tsx
+++ app/Labs/Lab3/SimpleArrays.tsx
@@ -1,5 +1,5 @@
 export default function SimpleArrays() {
-    let functionScoped = 2;  const blockScoped = 5;
+    const functionScoped = 2;  const blockScoped = 5;
     const constant1 = functionScoped - blockScoped;
     const numberArray1 = [1, 2, 3, 4, 5];
     const stringArray1 = ["string1", "string2"];
--- app/Labs/Lab3/VariablesAndConstants.tsx
+++ app/Labs/Lab3/VariablesAndConstants.tsx
@@ -1,5 +1,5 @@
 export default function VariablesAndConstants() {
-    let functionScoped = 2;
+    const functionScoped = 2;
     const blockScoped = 5;
     const constant1 = functionScoped - blockScoped;
     return(
