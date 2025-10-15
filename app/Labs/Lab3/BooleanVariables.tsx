2ed3988d92b308303ecf94684cbf266a846b01b1
weihang-z <weihangzeng79@gmail.com>
2025-10-14 23:55:49 -0400

    fix ts errors

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -1,4 +1,4 @@
-const numberVariable = 123, floatingPointNumber = 234.345;
+const numberVariable: number = 123, floatingPointNumber: number = 234.345;
 const true1 = true, false1 = false;
 const false2 = true1 && false1;
 const true2 = true1 || false1;
